c43c132bfe52408efe830d1d9adedbede8809f4c
cierravereen24 <cierravereen24@gmail.com>
2019-01-23 14:33:14 -0600

    re-uploaded cw

---
+++ loops_review.py
@@ -0,0 +1,52 @@
+def main():
+    #exercise1()
+    #exercise2()
+
+# exercise1
+# Write a Python program that prints all the numbers from 0 to 6 except 3 and 6.
+#
+# Hint: Use 'continue' statement.
+#
+# Expected Output :
+# ```0 1 2 4 5```
+
+# def exercise1():
+#     for nums in range(6):
+#         if int(nums != 3 and nums != 6):
+#             print(nums)
+#
+#
+# if __name__ == '__main__':
+#     main()
+
+# EXERCISE2
+# Write a Python program to count the number of even and odd numbers from a series of numbers.
+# Sample numbers: ```numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9)```
+# Expected Output :
+# ```
+# Number of even numbers : 5
+# Number of odd numbers : 4
+# ```
+
+# def exercise2():
+#     even = 0
+#     odd = 0
+#     for number in range(0,10):
+#        if number % 2 == 0:
+#            even += 1
+#        else:
+#            odd += 1
+#     print("Number of even numbers:", even)
+#     print("Number of odd numbers:", odd)
+#
+# if __name__ == '__main__':
+#     main()
+
+# EXERCISE3
+# Write a Python program that accepts a sequence of lines (blank line to terminate) as input and prints the lines as output after User enters a blank line to end.
+# Do not use an array to hold the lines of text
+# Hints:
+# You can use ```"\n"``` if you want to add a line break between sentences
+
+
+def exercise3():
